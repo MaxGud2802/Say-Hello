267eae77e858b902e3973bff219992675887aa29
Maximus Gudino <m_gudino@swbell.net>
2021-12-21 16:19:39 -0600

    the layout on the screen is properly fitted to change size according to device and device orientation

--- Say Hello/ViewController.swift
+++ Say Hello/ViewController.swift
@@ -8,7 +8,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var viewBackground: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
